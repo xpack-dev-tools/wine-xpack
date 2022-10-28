2a156276ec81a64e7e9b483c055aaf07d7899213
Liviu Ionescu <ilg@livius.net>
2022-10-28 21:51:26 +0300

    VERSION 7.0.0-1

--- scripts/versioning.sh
+++ scripts/versioning.sh
@@ -24,23 +24,46 @@ function build_application_versioned_components()
   fi
 
   # Keep them in sync with combo archive content.
-  if [[ "${XBB_RELEASE_VERSION}" =~ 6\.17\.* ]]
+  if [[ "${XBB_RELEASE_VERSION}" =~ 7\.*\.* ]]
   then
     # -------------------------------------------------------------------------
 
     xbb_set_binaries_install "${XBB_DEPENDENCIES_INSTALL_FOLDER_PATH}"
     xbb_set_libraries_install "${XBB_DEPENDENCIES_INSTALL_FOLDER_PATH}"
 
-    # No dependencies.
+    # https://sourceforge.net/projects/libpng/files/libpng16/
+    build_libpng "1.6.38" # "1.6.37"
+
+    xbb_set_binaries_install "${XBB_APPLICATION_INSTALL_FOLDER_PATH}"
+
+    # the xPack gcc does not support -m32 yet.
+    XBB_WINE_SKIP_WIN32="y"
+
+    # https://dl.winehq.org/wine/source/
+    build_wine "${XBB_WINE_VERSION}"
+
+    run_verbose rm -rfv "${XBB_APPLICATION_INSTALL_FOLDER_PATH}/share/man"
+
+    # -------------------------------------------------------------------------
+  elif [[ "${XBB_RELEASE_VERSION}" =~ 6\.17\.* ]]
+  then
+    # -------------------------------------------------------------------------
+
+    xbb_set_binaries_install "${XBB_DEPENDENCIES_INSTALL_FOLDER_PATH}"
+    xbb_set_libraries_install "${XBB_DEPENDENCIES_INSTALL_FOLDER_PATH}"
+
+    # https://sourceforge.net/projects/libpng/files/libpng16/
+    build_libpng "1.6.37"
 
     xbb_set_binaries_install "${XBB_APPLICATION_INSTALL_FOLDER_PATH}"
 
     # i686-w64-mingw32-gcc not available in the Docker container.
     XBB_WINE_SKIP_WIN32="y"
 
+    # https://dl.winehq.org/wine/source/
     build_wine "${XBB_WINE_VERSION}"
 
-    run_verbose rm -rfv "${APP_INSTALL_FOLDER_PATH}/share/man"
+    run_verbose rm -rfv "${XBB_APPLICATION_INSTALL_FOLDER_PATH}/share/man"
 
     # -------------------------------------------------------------------------
   else
