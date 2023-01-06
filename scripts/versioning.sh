e44aa1014bf3bb3c4f5adc0d0de2b2ddbf7d584d
Liviu Ionescu <ilg@livius.net>
2023-01-06 13:21:39 +0200

    versioning.sh: no need to build png

--- scripts/versioning.sh
+++ scripts/versioning.sh
@@ -34,7 +34,8 @@ function application_build_versioned_components()
     xbb_set_target "requested"
 
     # https://sourceforge.net/projects/libpng/files/libpng16/
-    libpng_build "1.6.39"
+    # libpng_build "1.6.39"
+
     # -------------------------------------------------------------------------
     # Build the application binaries.
 
