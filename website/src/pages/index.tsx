62ad25daac49789a442b3dc1f873466861168782
Liviu Ionescu <ilg@livius.net>
2024-10-10 01:33:36 +0300

    website: updates

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -43,7 +43,7 @@ export default function Home(): JSX.Element {
   return (
     <Layout
       title={`Hello from xPack Binary Development Tools`}
-      description="A binary xPack with the WineHQ executables">
+      description="A binary package with the WineHQ executables">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
