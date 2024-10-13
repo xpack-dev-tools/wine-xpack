574bdd59dbbdb11f7739f09d80c3790f44506021
Liviu Ionescu <ilg@livius.net>
2024-10-14 00:34:24 +0300

    website: updates

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -42,7 +42,7 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout 
-      title={`Hello from wine`}
+      title={`Welcome to wine`}
       description="A binary package with the wine executables">
       <HomepageHeader />
       <main>
