18668346a89e430137ef35df6b7a958d287bfa66
Liviu Ionescu <ilg@livius.net>
2025-01-21 15:23:09 +0200

    website: re-generate commons

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -34,7 +34,7 @@ function HomepageHeader() {
         <p className="hero__subtitle">{siteConfig.tagline}</p>
 
         <div className={styles.installWithCopy}>
-          <InstallWithCopy>xpm install @xpack-dev-tools/wine@{customField('releaseVersion')} --verbose</InstallWithCopy>
+          <InstallWithCopy>xpm install @xpack-dev-tools/wine@9.0.0-1.1 --verbose</InstallWithCopy>
         </div>
 
       </div>
