a75105045f62e31ea1fdd5484773aca2fed58f55
Liviu Ionescu <ilg@livius.net>
2025-01-30 10:55:22 +0200

    website: more rework with commons

--- website/customFields.ts
+++ website/customFields.ts
@@ -24,6 +24,7 @@ export function getCustomFields() {
   const releaseSemver = releaseVersion.replace(/[-].*$/, '');
 
   let upstreamVersion = releaseSemver;
+
   if (releaseSemver.endsWith('.0')) {
     // Remove the patch number if zero (wine uses both 2 and 3 numbers).
     upstreamVersion = releaseSemver.replace(/[.]0*$/, '');
